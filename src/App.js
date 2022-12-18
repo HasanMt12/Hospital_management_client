19e67b9fff6ac1c9076ef8d2b0a3c10ea6a0d7a9
Rakibul Islam <rakib011110@gmail.com>
2022-12-18 11:56:14 +0600

    add

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Routes';
-import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div className='w-11/12 mx-auto '>
+    <div>
       <RouterProvider router={router}></RouterProvider>
-      <Toaster position="top-center" reverseOrder={false}></Toaster>
     </div>
   );
 }
