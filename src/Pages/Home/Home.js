a8e111ec7a7011a4ce0327bed9112583fd72cfe4
nafisamou <108416863+nafisamou@users.noreply.github.com>
2022-12-18 10:18:27 +0600

    made donation form

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { experimentalStyled as styled } from '@mui/material/styles';
 import Services from './Services/Services';
 
+// import { CssBaseline } from "@material-ui/core";
+import Carousel from './Carousel/Carousel';
+import DonationForm from './Donate/DonationForm';
+
+
 const Home = () => {
     return (
         <div>
             <h1>this is home</h1>
+            
+              
+            <Carousel></Carousel>
             <Services></Services>
+            <DonationForm></DonationForm>
+            {/* <CssBaseline /> */}
         </div>
     );
 };
