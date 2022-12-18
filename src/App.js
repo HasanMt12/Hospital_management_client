80650725e3461d15a6b137e15dbf97278699758c
Rakibul Islam <rakib011110@gmail.com>
2022-12-18 11:52:13 +0600

    service add

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Routes';
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <div>
+    <div className='w-11/12 mx-auto '>
       <RouterProvider router={router}></RouterProvider>
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
     </div>
   );
 }
