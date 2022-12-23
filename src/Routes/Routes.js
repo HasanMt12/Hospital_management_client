0464cf133f86972c6c8a568e9fb96a4e876ed0f1
rawfoon <mgrawfoon@gmail.com>
2022-12-23 19:54:24 +0600

    service and doctor adedd

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "../Layout/DashboardLayout";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
+import ServicesPage from "../Pages/Home/Services/AllServices/ServicesPage";
+import Service from "../Pages/Home/Services/Service";
 import Login from "../Pages/Register/Login/Login";
 import SignUp from "../Pages/Register/SignUp/SignUp";
 import ErrorPage from "../Pages/Shared/ErrorPage";
@@ -23,6 +25,10 @@ const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login></Login>
+            }, 
+            {
+                path: '/services',
+                element: <ServicesPage></ServicesPage>
             } 
         ]
     },
