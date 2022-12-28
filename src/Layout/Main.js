c29767537447efc6054bfb6dec9f10deebb03c7a
suhanasalma <108382046+suhanasalma@users.noreply.github.com>
2022-12-28 12:51:46 +0600

    header issue need to solve

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -4,7 +4,10 @@ import Footer from '../Pages/Shared/Footer/Footer';
 import Header from '../Pages/Shared/Header/Header';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
-
+// header issue need to solve
+// header issue need to solve
+// header issue need to solve
+// header issue need to solve
 
 const Main = () => {
   return (
